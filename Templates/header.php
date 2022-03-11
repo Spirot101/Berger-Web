cdc0daf99dfb63b1aaa0a071c2231b1e68b72dec
Erik Berger <erik.berger12@hotmail.com>
2022-03-11 15:59:28 +0100

    major overhaul

--- Templates/header.php
+++ Templates/header.php
@@ -4,6 +4,6 @@
 			<ul>			
 				<li><a class="head-list" href="../login/login.html"><i class="material-icons">login</i> Login</a></li>
 		
-				<li><a class="head-list" href="../Registration/registration.php"><i class="material-icons">account_circle</i> Create Account</a></li>			
+				<li><a class="head-list" href="../Registration/registration.php"><i class="material-icons">account_circle</i> Register Account</a></li>			
 			</ul>
 		</div>
--- php-blog/includes/navbar.php
+++ php-blog/includes/navbar.php
@@ -23,7 +23,19 @@
 
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <!-- ändra färg på home här -->
+          <a class="nav-link text-white active" aria-current="page" href="../index.html">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white active" aria-current="page" href="#">Forum</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white active" aria-current="page" href="../gallery/gallery.php">Gallery</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white active" aria-current="page" href="../contact/contact.php">Contact</a>
+        </li>
+        <li class="nav-item">
+          <!-- ändra färg här -->
           <a class="nav-link text-white active" aria-current="page" href="index.php">Blog</a>
         </li>
         <?php
